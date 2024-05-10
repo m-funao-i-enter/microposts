1304ce76e1bf853f217a572aaaea46cc3f8f513c
m-funao <m-funao@i-enter.co.jp>
2024-05-10 05:58:34 +0000

    user pages

--- microposts/resources/views/layouts/app.blade.php
+++ microposts/resources/views/layouts/app.blade.php
@@ -2,8 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <title>Microposts</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        @vite('resources/css/app.css')
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -14,23 +16,18 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
+    
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        
+        {{-- ナビゲーションバー --}}
+        @include('commons.navbar')
+        
+        <div class="container mx-auto">
+            {{-- エラーメッセージ --}}
+            @include('commons.error_messages')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            @yield('content')
         </div>
+        
     </body>
 </html>
